45c1ddbf3dc788e645e6af70bf6e4f3a4988038c
Daniel Joseph <danieljoseph@Daniels-MacBook-Pro.local>
2026-09-02 15:42:34 +0100

    v2

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-
+from datetime import datetime
 failed_attempts = {}
 
 with open("login_logs.txt", "r") as file:
     for line in file:
-        username, ip, status = line.strip().split(',')
+        timestamp_text, username, ip, status = line.strip().split(',')
+
+        timestamp = datetime.strptime(timestamp_text, "%Y-%m-%d %H:%M:%S")
+        print(timestamp)
 
         if status == "failed":
             if ip in failed_attempts:
                 failed_attempts[ip] += 1
             else:
                 failed_attempts[ip] = 1
+
+            failed_attempts[ip].append(timestamp)
+
+
+
 
 for ip, attempts in failed_attempts.items():
     if attempts >= 5:
@@ -18,6 +26,8 @@ for ip, attempts in failed_attempts.items():
         print(f"IP: {ip}")
         print(f"Failed attempts: {attempts}")
         print("Risk: HIGH")
+    elif attempts >= 3:
+        print("Risk: MEDIUM")
 
 
 
